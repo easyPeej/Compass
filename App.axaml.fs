d78f2a17ec2889a8d4bd0f582991099b6537cabb
PJ Scott-Caven <94923851+easyPeej@users.noreply.github.com>
2025-02-17 12:40:25 +0000

    First

---
+++ App.axaml.fs
@@ -0,0 +1,22 @@
+namespace Compass
+
+open Avalonia
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Markup.Xaml
+open Database
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+            AvaloniaXamlLoader.Load(this)
+
+    override this.OnFrameworkInitializationCompleted() =
+        initializeDatabase() 
+        
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktop ->
+             desktop.MainWindow <- MainWindow()
+        | _ -> ()
+
+        base.OnFrameworkInitializationCompleted()
---
+++ Database.fs
@@ -0,0 +1,29 @@
+﻿namespace Compass
+open System
+open Microsoft.Data.Sqlite
+
+module Database =
+    
+    (* This let block is an unfinished attempt at storing the database in a more secure location eg AppData - only accessible via administrative users. *)
+    
+    (*let dbPath=
+        let basePath =
+            if OperatingSystem.IsWindows() then
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+            else 
+                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local", "share")
+        Path.Combine(basePath, "Compass", "app.db")
+            
+    let connectionString = $"Data Source={dbPath}"    *)    
+    
+    
+    let initializeDatabase () =
+        use connection = new SqliteConnection("Data Source=Data.db")
+        connection.Open()
+        use command = connection.CreateCommand()
+        command.CommandText <- "
+            CREATE TABLE IF NOT EXISTS Users (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL
+            )"
+        command.ExecuteNonQuery() |> ignore
